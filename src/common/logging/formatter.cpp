cb31556487beb6ed0bffad91f1d6c23ae6fed037
James Rowe <jroweboy@gmail.com>
2017-05-09 21:23:33 -0600

    Logging: Add custom formatter

---
+++ src/common/logging/formatter.cpp
@@ -0,0 +1,44 @@
+#include "common/assert.h"
+#include "common/logging/formatter.h"
+
+namespace Log {
+
+Formatter::Formatter() {}
+
+static const char* GetLevelName(spdlog::level_t log_level) {
+    switch (log_level) {
+    case spdlog::level::trace:
+        return "Trace";
+    case spdlog::level::debug:
+        return "Debug";
+    case spdlog::level::info:
+        return "Info";
+    case spdlog::level::warn:
+        return "Warn";
+    case spdlog::level::err:
+        return "Error";
+    case spdlog::level::critical:
+        return "Critical";
+    default:
+        UNREACHABLE();
+    }
+}
+
+void Formatter::format(spdlog::details::log_msg& msg) {
+    using std::chrono::steady_clock;
+    using std::chrono::duration_cast;
+
+    static steady_clock::time_point time_origin = steady_clock::now();
+    auto timestamp = duration_cast<std::chrono::microseconds>(steady_clock::now() - time_origin);
+
+    unsigned int time_seconds = static_cast<unsigned int>(timestamp.count() / 1000000);
+    unsigned int time_fractional = static_cast<unsigned int>(timestamp.count() % 1000000);
+
+    msg.formatted << '[' << fmt::pad(time_seconds, ' ', 4) << '.'
+                  << fmt::pad(time_fractional, '0', 6) << "] ";
+    msg.formatted << *msg.logger_name << " <" << GetLevelName(msg.level) << "> ";
+
+    msg.formatted << fmt::StringRef(msg.raw.data(), msg.raw.size());
+}
+
+} // namespace Log
---
+++ src/common/logging/formatter.h
@@ -0,0 +1,15 @@
+#include <string>
+#include <spdlog/spdlog.h>
+
+namespace Log {
+
+class Formatter : public spdlog::formatter {
+
+public:
+    explicit Formatter();
+    Formatter(const Formatter&) = delete;
+    Formatter& operator=(const Formatter&) = delete;
+    void format(spdlog::details::log_msg& msg) override;
+};
+
+} // namespace Log
