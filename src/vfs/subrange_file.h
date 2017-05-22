52c03480914614de5a8c03c45141c92b02b8551f
Yuri Kunde Schlesner <yuriks@yuriks.net>
2017-05-21 20:31:53 -0700

    VFS: Add SubrangeFile, a window into another File

---
+++ src/vfs/subrange_file.h
@@ -0,0 +1,37 @@
+// Copyright 2017 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <limits>
+#include "vfs/file.h"
+
+namespace Vfs {
+
+class SubrangeFile : public File {
+public:
+    static constexpr u64 UNBOUNDED_SIZE = std::numeric_limits<u64>::max();
+
+    SubrangeFile(std::shared_ptr<File> base_file, u64 offset, u64 size);
+
+    void DebugFmt(fmt::Writer& writer) const override;
+
+    ResultVal<size_t> Read(u64 offset, size_t length, u8* buffer) override;
+    ResultVal<size_t> Write(u64 offset, size_t length, const u8* buffer) override;
+    ResultVal<u64> GetSize() override;
+    ResultCode SetSize(u64 size) override;
+    ResultCode Close() override;
+    ResultCode Flush() override;
+
+    std::shared_ptr<File> GetBaseFile() const {
+        return base_file;
+    }
+
+private:
+    std::shared_ptr<File> base_file;
+    u64 file_offset;
+    u64 file_size;
+};
+
+} // namespace Vfs
