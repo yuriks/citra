b7edd43080970401b8bd1ff674304b1b774ad4f5
Yuri Kunde Schlesner <yuriks@yuriks.net>
2018-01-19 23:55:16 -0800

    VFS: Add StreamFile
    StreamFile is an interface representing stream-oriented files, which
    have a read-write cursor instead of allowing free random access.

--- src/vfs/file.h
+++ src/vfs/file.h
@@ -71,4 +71,55 @@ public:
     }
 };
 
+class StreamFile : public Common::VirtualPrintable {
+public:
+    virtual ~StreamFile() = default;
+
+    /**
+     * Reads data from the file and advances the cursor by the amount read.
+     *
+     * @param length number of bytes to read
+     * @param buffer buffer to receive the read data
+     * @return number of bytes actually read, or ERR_END_OF_FILE if at EOF
+     */
+    virtual ResultVal<size_t> Read(size_t length, u8* buffer) = 0;
+
+    /**
+     * Writes data to the file and advances the cursor by the amount written.
+     *
+     * @param length number of bytes to write
+     * @param buffer buffer to write data from
+     * @return number of bytes actually written
+     */
+    virtual ResultVal<size_t> Write(size_t length, const u8* buffer) = 0;
+
+    /**
+     * Moves the read/write cursor to a different position in the file.
+     *
+     * @param offset_from_beginning offset inside the file to move the cursor to
+     * @return ERR_END_OF_FILE if position is outside current file bounds
+     */
+    virtual ResultCode Seek(u64 offset_from_beginning) = 0;
+
+    /** Returns the position of the read/write cursor in bytes from the beginning of the file. */
+    virtual ResultVal<u64> Tell() = 0;
+
+    /** Returns the size of the file in bytes. */
+    virtual ResultVal<u64> GetSize() const = 0;
+
+    /**
+     * Changes the size of the file. If smaller than the current size, the file will be truncated.
+     * If larger, the new space will be filled with zeros.
+     *
+     * @param size new size of the file, in bytes
+     */
+    virtual ResultCode SetSize(u64 size) = 0;
+
+    /** Closes the file. */
+    virtual ResultCode Close() = 0;
+
+    /** Flushes any pending writes. */
+    virtual ResultCode Flush() = 0;
+};
+
 } // namespace Vfs
